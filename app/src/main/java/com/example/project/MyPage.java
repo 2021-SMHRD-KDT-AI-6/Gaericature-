300ead0ad318fd6efcfd8dcf173af6910f64d9d4
dongsun1 <le12352@naver.com>
2022-01-04 18:30:18 +0900

    mypage ui 절반

--- app/src/main/java/com/example/project/MyPage.java
+++ app/src/main/java/com/example/project/MyPage.java
@@ -1,18 +1,29 @@
 package com.example.project;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class MyPage extends Fragment {
+
+    ImageView imgProfile;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View fragment = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        imgProfile = fragment.findViewById(R.id.imgProfile);
+
+        imgProfile.setBackground(new ShapeDrawable(new OvalShape()));
+        imgProfile.setClipToOutline(true);
 
         return fragment;
     }
