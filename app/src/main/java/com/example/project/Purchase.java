5f6f9d2607b36c16e959faac8adb4dbbc49382b7
sunjuchoi123123 <sunju5389@gmail.com>
2022-01-05 15:20:04 +0900

    DB 연동 1차

--- app/src/main/java/com/example/project/Purchase.java
+++ app/src/main/java/com/example/project/Purchase.java
@@ -4,16 +4,80 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Purchase extends Fragment {
+
+    TextView tv;
+    Button btn;
+    RequestQueue requestQueue;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View fragment = inflater.inflate(R.layout.fragment_purchase, container, false);
 
+        tv = fragment.findViewById(R.id.tv);
+        btn = fragment.findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                String url = "http://192.168.0.115:8081/Gaericature/testController";
+
+                StringRequest request = new StringRequest(
+                        Request.Method.POST,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (response.equals("1")) {
+                                    tv.setText("성공");
+                                } else {
+                                    tv.setText("실패");
+
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                ){
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        String a = "2";
+                        params.put("num", a);
+
+                        return params;
+                    }
+                };
+                requestQueue.add(request);
+            }
+        });
         return fragment;
     }
 }
