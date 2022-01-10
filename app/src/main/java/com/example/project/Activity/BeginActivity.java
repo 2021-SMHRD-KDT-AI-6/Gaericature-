c166dffd0e7b8bee723bd31557df5cf6524b7cfd
dongsun1 <le12352@naver.com>
2022-01-10 10:25:15 +0900

    시작페이지

---
+++ app/src/main/java/com/example/project/Activity/BeginActivity.java
@@ -0,0 +1,91 @@
+package com.example.project.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.project.Adapter.MyGaericatureAdapter;
+import com.example.project.R;
+import com.example.project.VO.MyGaericatureVO;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class BeginActivity extends AppCompatActivity {
+
+    EditText edId, edPw;
+    Button btnLogin, btnSignUp;
+    String url = "http://172.30.1.12:8081/Gaericature/loginController";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+
+        edId = findViewById(R.id.edId);
+        edPw = findViewById(R.id.edPw);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = new FormBody.Builder()
+                        .add("user_id", String.valueOf(edId.getText()))
+                        .add("user_pw", String.valueOf(edPw.getText()))
+                        .build();
+
+                Request request = new Request.Builder().url(url).post(body).build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        String result = response.body().string();
+                        if (result.equals("1")){
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Handler handler = new Handler(Looper.getMainLooper());
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                                }
+                            }, 0);
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/project/Activity/MyPagePurchaseAllHistory.java
+++ app/src/main/java/com/example/project/Activity/MyPagePurchaseAllHistory.java
@@ -3,12 +3,23 @@ package com.example.project.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.project.R;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MyPagePurchaseAllHistory extends AppCompatActivity {
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +30,37 @@ public class MyPagePurchaseAllHistory extends AppCompatActivity {
         String PurchaseAllNum = intent.getExtras().getString("PurchaseAllNum");
         TextView tvAll = findViewById(R.id.tvAll);
         tvAll.setText(PurchaseAllNum);
+        ImageView img = findViewById(R.id.img);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    URL url = new URL("http://172.30.1.12/C:/Users/smhrd/project2/고양이.jpg");
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setDoInput(true);
+
+                    conn.connect();
+
+                    InputStream inputStream = conn.getInputStream();
+
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
+        try {
+            thread.join();
+            img.setImageBitmap(bitmap);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
